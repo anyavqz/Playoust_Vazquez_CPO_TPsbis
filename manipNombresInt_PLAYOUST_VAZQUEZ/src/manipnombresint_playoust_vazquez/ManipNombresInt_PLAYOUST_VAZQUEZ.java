3aff6221f021420a843fd2fd991ad195d98e3e9a
anyavazquez <anyavazquez@192.168.114.142>
2021-10-04 16:48:36 +0200

    Nouveau projet manipInt

---
+++ manipNombresInt_PLAYOUST_VAZQUEZ/src/manipnombresint_playoust_vazquez/ManipNombresInt_PLAYOUST_VAZQUEZ.java
@@ -0,0 +1,22 @@
+/*
+Vazquez Anya - Prosper Playoust
+TDB-2
+27/09/2021
+TP1
+ */
+package manipnombresint_playoust_vazquez;
+
+/**
+ *
+ * @author anyavazquez
+ */
+public class ManipNombresInt_PLAYOUST_VAZQUEZ {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
