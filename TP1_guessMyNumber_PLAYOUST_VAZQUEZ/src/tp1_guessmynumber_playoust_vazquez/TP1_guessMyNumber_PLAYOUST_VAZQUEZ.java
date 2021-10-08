6db6a6346b36e7d0a9298e7de1c4602b52c93d70
petitweb <petitweb@192.168.1.7>
2021-10-08 22:30:58 +0200

    Exercice presque fini

--- TP1_guessMyNumber_PLAYOUST_VAZQUEZ/src/tp1_guessmynumber_playoust_vazquez/TP1_guessMyNumber_PLAYOUST_VAZQUEZ.java
+++ TP1_guessMyNumber_PLAYOUST_VAZQUEZ/src/tp1_guessmynumber_playoust_vazquez/TP1_guessMyNumber_PLAYOUST_VAZQUEZ.java
@@ -7,6 +7,7 @@ TP1 - exo2
 package tp1_guessmynumber_playoust_vazquez;
 
 import java.util.Random;
+import java.util.Scanner;
 
 /**
  *
@@ -19,7 +20,81 @@ public class TP1_guessMyNumber_PLAYOUST_VAZQUEZ {
      */
     public static void main(String[] args) {
         Random generateurAleat = new Random ();
-        System.out.println(generateurAleat);
-    }
+        int Nb1=generateurAleat.nextInt(100);
+        int Nb2=generateurAleat.nextInt(100);
+        int Nb3=generateurAleat.nextInt(100);
+        int Nb4=generateurAleat.nextInt(100);
+        int Nb5=generateurAleat.nextInt(100);
+        System.out.println(Nb1);
+        System.out.println(Nb2);
+        System.out.println(Nb3);
+        System.out.println(Nb4);
+        System.out.println(Nb5);
+        
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Quel niveau de difficulté vous voulez ?\n1)Facile\n2)Normal\n3)Difficile");
+        int Diff=sc.nextInt();
+        int NbAl=generateurAleat.nextInt(100);
+        int NbUt=-1;
+        int compteur=0;
+        if (Diff==2) {
+            while (NbUt!=NbAl) {
+                System.out.println("Veuillez saisir un nombre");
+                NbUt=sc.nextInt();
+                compteur+=1;
+                if (NbUt>NbAl) {
+                    System.out.println("Trop grand");
+                }
+                if (NbUt<NbAl) {
+                System.out.println("Trop petit");
+            }
+     
+            }System.out.println("Gagné");
+             System.out.println("Vous avez fait "+compteur+" tentatives");
+        }
+        if (Diff==1) {
+            NbAl=generateurAleat.nextInt(75);
+            while (NbUt!=NbAl) {
+                System.out.println("Veuillez saisir un nombre");
+                NbUt=sc.nextInt();
+                compteur+=1;
+                if(NbUt>NbAl+25) {
+                    System.out.println("Vraiment trop grand");
+                }
+                if (NbUt>NbAl && NbUt<NbAl+25) {
+                    System.out.println("Trop grand");
+                }
+                if(NbUt<NbAl-25) {
+                    System.out.println("Vraiment trop petit");    
+                }
+                if(NbUt<NbAl && NbUt>NbAl-25) {
+                    System.out.println("Trop petit");
+                }
+            } 
+            System.out.println("Gagné");
+        }    
+        if (Diff==3) {
+            NbAl=generateurAleat.nextInt(150);
+            while (NbUt!=NbAl) {
+                System.out.println("Veuillez saisir un nombre");
+                NbUt=sc.nextInt();
+                compteur+=1;
+                
+                if (NbUt>NbAl) {
+                    System.out.println("Trop grand");
+                }
+                
+                if(NbUt<NbAl) {
+                    System.out.println("Trop petit");
+                }
+                if (compteur==10) {
+                    break;
+                }
+                System.out.println("Gagné");
+            }
+            
+        }
     
+}// Modifier 3
 }
