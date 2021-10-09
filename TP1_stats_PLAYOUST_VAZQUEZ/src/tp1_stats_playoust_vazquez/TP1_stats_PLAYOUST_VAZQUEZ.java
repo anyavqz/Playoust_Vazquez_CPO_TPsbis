8fd2adc1bc498c8d23e3d0a29c8a7cce8b044c2a
anyavazquez <anyavazquez@192.168.114.142>
2021-10-10 00:16:22 +0200

    Exercice 4 presque terminé

--- TP1_stats_PLAYOUST_VAZQUEZ/src/tp1_stats_playoust_vazquez/TP1_stats_PLAYOUST_VAZQUEZ.java
+++ TP1_stats_PLAYOUST_VAZQUEZ/src/tp1_stats_playoust_vazquez/TP1_stats_PLAYOUST_VAZQUEZ.java
@@ -20,25 +20,25 @@ public class TP1_stats_PLAYOUST_VAZQUEZ {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int [] Tab = new int [6];
-        int m;
-        for (int i=0; i < 6; i++) {
+        int [] Tab = new int [6]; //on déclare un tableau "Tab" de 6 cases qui contiendront des nombres entiers
+        int m; //déclaration de la variable m qui va être choisie par l'utilisateur
+        for (int i=0; i < 6; i++) { // ici, on créé une boucle for pour initialiser chaque case du tableau à 0
             Tab[i]=0;
         }
-        //System.out.println(Arrays.toString(Tab));
-        System.out.println("Veuillez saisir une valeur entière");
+        //System.out.println(Arrays.toString(Tab)); nous vérifions ici que la boucle for ci-dessus fonctionnait bien
+        System.out.println("Veuillez saisir une valeur entière");// affichage d'un message à l'utilisateur 
         Scanner sc;
-        sc = new Scanner(System.in); 
-        m=sc.nextInt();
+        sc = new Scanner(System.in); // Scanner permet à l'utilisateur de saisir des valeurs, et "new" permet qu'elles soient enregistrées
+        m=sc.nextInt(); // saisie de la valeur entière
         
-        for (int i=0; i <= m; i++) {
-            Random generateurAleat = new Random ();
-            int Nb1=generateurAleat.nextInt(5);
-            Tab[i]=Nb1;
+        for (int i=0; i <= m; i++) { //création de la boucle for pour i compris entre 0 et m
+            Random generateurAleat = new Random ();//générateur de nombre
+            int Nb1=generateurAleat.nextInt(5); //déclaration de la variable Nb1 et attribution d'une valeur aléatoire entre 0 et 5
+            Tab[Nb1]=i;// la case d'indice correspondant est incrémentée de la valeur de i ???????? bizarre
         }
         System.out.println("Le tableau résultat est: ");
         for (int i=0; i < 6; i++) {
-            System.out.println(Tab[i]);
+            System.out.println(Tab[i]); // affichage du tableau résultat à l'aide d'une boucle for
         }
     }
     
