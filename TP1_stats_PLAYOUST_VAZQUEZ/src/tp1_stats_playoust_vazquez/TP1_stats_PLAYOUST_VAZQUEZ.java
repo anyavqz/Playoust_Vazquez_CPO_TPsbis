37bfb3b572e3551031c3ee1ae0548f0e793cc685
anyavazquez <anyavazquez@192.168.114.142>
2021-10-10 00:00:08 +0200

    Exercice 4 presque terminé

--- TP1_stats_PLAYOUST_VAZQUEZ/src/tp1_stats_playoust_vazquez/TP1_stats_PLAYOUST_VAZQUEZ.java
+++ TP1_stats_PLAYOUST_VAZQUEZ/src/tp1_stats_playoust_vazquez/TP1_stats_PLAYOUST_VAZQUEZ.java
@@ -1,8 +1,14 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+Vazquez Anya - Prosper Playoust
+TDB-2
+27/09/2021
+TP1 - exo4
  */
 package tp1_stats_playoust_vazquez;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
 
 /**
  *
@@ -14,7 +20,26 @@ public class TP1_stats_PLAYOUST_VAZQUEZ {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int [] Tab = new int [6];
+        int m;
+        for (int i=0; i < 6; i++) {
+            Tab[i]=0;
+        }
+        //System.out.println(Arrays.toString(Tab));
+        System.out.println("Veuillez saisir une valeur entière");
+        Scanner sc;
+        sc = new Scanner(System.in); 
+        m=sc.nextInt();
+        
+        for (int i=0; i <= m; i++) {
+            Random generateurAleat = new Random ();
+            int Nb1=generateurAleat.nextInt(5);
+            Tab[i]=Nb1;
+        }
+        System.out.println("Le tableau résultat est: ");
+        for (int i=0; i < 6; i++) {
+            System.out.println(Tab[i]);
+        }
     }
     
 }
