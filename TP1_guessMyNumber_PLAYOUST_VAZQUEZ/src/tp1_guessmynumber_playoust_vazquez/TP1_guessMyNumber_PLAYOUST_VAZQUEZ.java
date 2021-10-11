d33f2770fd16eb3364330ef982a30d9da3e22a70
anyavazquez <anyavazquez@192.168.114.142>
2021-10-11 13:55:48 +0200

    Commentaire GuessMyNumber

--- TP1_guessMyNumber_PLAYOUST_VAZQUEZ/src/tp1_guessmynumber_playoust_vazquez/TP1_guessMyNumber_PLAYOUST_VAZQUEZ.java
+++ TP1_guessMyNumber_PLAYOUST_VAZQUEZ/src/tp1_guessmynumber_playoust_vazquez/TP1_guessMyNumber_PLAYOUST_VAZQUEZ.java
@@ -19,8 +19,8 @@ public class TP1_guessMyNumber_PLAYOUST_VAZQUEZ {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Random generateurAleat = new Random ();
-        int Nb1=generateurAleat.nextInt(100);
+        Random generateurAleat = new Random (); //Creation du generateur de nombre aleatoire
+        int Nb1=generateurAleat.nextInt(100); // Creation d'un nombre aleatoire entre 0 et 100
         int Nb2=generateurAleat.nextInt(100);
         int Nb3=generateurAleat.nextInt(100);
         int Nb4=generateurAleat.nextInt(100);
@@ -34,32 +34,32 @@ public class TP1_guessMyNumber_PLAYOUST_VAZQUEZ {
         
         Scanner sc = new Scanner(System.in);
         System.out.println("Quel niveau de difficulté vous voulez ?\n1)Facile\n2)Normal\n3)Difficile");
-        int Diff=sc.nextInt();
-        int NbAl=generateurAleat.nextInt(100);
-        int NbUt=-1;
-        int compteur=0;
-        if (Diff==2) {
+        int Diff=sc.nextInt(); 
+        int NbAl=generateurAleat.nextInt(100); //On genere un nombre aleatoire entre 0 et 100 qui devra etre devine par l'utilisateur
+        int NbUt=-1; // On initialise la variable qui va prendre le nombre saisi par l'utilisateur
+        int compteur=0;// On initialise le compteur
+        if (Diff==2) { 
             while (NbUt!=NbAl) {
                 System.out.println("Veuillez saisir un nombre");
-                NbUt=sc.nextInt();
-                compteur+=1;
-                if (NbUt>NbAl) {
-                    System.out.println("Trop grand");
+                NbUt=sc.nextInt(); // Tant que le nomnbre n'est pas deviné, on demande un autre nombre à l'utilisteur.
+                compteur+=1; // Le compteur augmente a chaque essai
+                if (NbUt>NbAl) { 
+                    System.out.println("Trop grand"); // On donne un indice
                 }
                 if (NbUt<NbAl) {
                 System.out.println("Trop petit");
             }
      
-            }System.out.println("Gagné");
+            }System.out.println("Gagné"); // Si on sort de la boucle, ca veut dire que c'est gagné
              System.out.println("Vous avez fait "+compteur+" tentatives");
         }
-        if (Diff==1) {
-            NbAl=generateurAleat.nextInt(75);
+        if (Diff==1) { 
+            NbAl=generateurAleat.nextInt(75); // On diminue l'intervalle du nombre aleatoire pour que ce soit plus simple
             while (NbUt!=NbAl) {
                 System.out.println("Veuillez saisir un nombre");
                 NbUt=sc.nextInt();
                 compteur+=1;
-                if(NbUt>NbAl+25) {
+                if(NbUt>NbAl+25) { // Si l'écart est plus grand que 25, on donne un meilleur indice.
                     System.out.println("Vraiment trop grand");
                 }
                 if (NbUt>NbAl && NbUt<NbAl+25) {
