a6e1ab17379ddb949f42a9b405c236b487a51ae2
anyavazquez <anyavazquez@192.168.114.142>
2021-10-22 14:47:13 +0200

    Début du projet

---
+++ TP2_Bieres_PLAYOUST_VAZQUEZ/src/tp2_bieres_playoust_vazquez/BouteilleBiere.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_bieres_playoust_vazquez;
+
+/**
+ *
+ * @author anyavazquez
+ */
+public class BouteilleBiere {
+    String Nom;
+    float degreAlcool;
+    
+}
