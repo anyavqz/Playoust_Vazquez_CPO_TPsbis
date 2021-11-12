ad504968bc8db5caa44275408d7241862b7e25a0
petitweb <petitweb@192.168.1.7>
2021-11-13 00:00:37 +0100

    TP2 partie 4

---
+++ TP2_relation_1_PLAYOUST_VAZQUEZ/src/tp2_relation_1_playoust_vazquez/Voiture.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp2_relation_1_playoust_vazquez;
+
+/**
+ *
+ * @author petitweb
+ */
+public class Voiture {
+    String Modele;
+    String Marque;
+    int PuissanceCV;
+    
+    public Voiture(String Mod, String Mq, int PCV) {
+        Modele = Mod;
+        Marque=Mq;
+        PuissanceCV = PCV;
+    } 
+    
+    @Override
+    public String toString () {
+        return Modele+" de la marque "+Marque+" de puissance "+PuissanceCV;
+    }
+}
