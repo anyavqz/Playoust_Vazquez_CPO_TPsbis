668a31f0b953a82c8ce5d26402d535b65f8dc8a0
petitweb <petitweb@192.168.1.7>
2021-11-12 14:07:51 +0100

    TP2 résolution des problèmes

---
+++ TP2_convertisseurObjet_PLAYOUST_VAZQUEZ/src/tp2_convertisseurobjet_playoust_vazquez/Convertisseur.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp2_convertisseurobjet_playoust_vazquez;
+/**
+ *
+ * @author petitweb
+ */
+public class Convertisseur {
+    int nbConversions;
+    
+    public Convertisseur() {
+        nbConversions=0;
+    }
+    
+    public float CelciusVersKelvin(float Celcius) { // On créé notre démarche. Elle prend en entrée une valeur en Celcius et retourne sa pendante en Kelvin
+        float Kelvin;
+        nbConversions+=1;
+        return Kelvin=Celcius+273.15f; // On ajoute un f derrière le nombre pour le forcer à être en float
+    }
+
+    public float KelvinVersCelcius(float Kelvin) { // Meme principe
+        float Celcius;
+        nbConversions+=1;
+        return Celcius=Kelvin-273.15f;
+         
+    }
+    
+    public float FarenheitVersCelcius(float Farenheit) {
+        float Celcius;
+        nbConversions+=1;
+        return Celcius=(Farenheit-32)/1.8f;
+    }
+    
+    public float CelciusVersFarenheit(float Celcius) {
+        float Farenheit;
+        nbConversions+=1;
+        return Farenheit=Celcius*1.8f+32f;
+    }
+    
+    public float KelvinVersFarenheit(float Kelvin) { // Pour cette demarche, on décide de faire appel à une autre démarche deja créée
+        float Farenheit;
+        float Celcius=KelvinVersCelcius(Kelvin);
+        nbConversions-=1; // On retire 1 car on utilise deux méthodes.
+        return Farenheit=CelciusVersFarenheit(Celcius);
+    }
+    
+    public float FarenheitVersKelvin(float Farenheit) { // Meme chose
+        float Kelvin;
+        float Celcius=FarenheitVersCelcius(Farenheit);
+        nbConversions-=1;
+        return Kelvin=CelciusVersKelvin(Celcius);
+    }
+    @Override
+    public String toString () {
+    return "nb de conversions"+ nbConversions;
+    }
+}
