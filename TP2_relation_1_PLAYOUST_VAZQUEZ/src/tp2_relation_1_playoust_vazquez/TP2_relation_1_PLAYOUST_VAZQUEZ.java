56237d80fd5cd01121de0e5414e907ac263186fd
petitweb <petitweb@192.168.1.7>
2021-11-13 18:31:00 +0100

    TP2 partie 4 finie

--- TP2_relation_1_PLAYOUST_VAZQUEZ/src/tp2_relation_1_playoust_vazquez/TP2_relation_1_PLAYOUST_VAZQUEZ.java
+++ TP2_relation_1_PLAYOUST_VAZQUEZ/src/tp2_relation_1_playoust_vazquez/TP2_relation_1_PLAYOUST_VAZQUEZ.java
@@ -21,7 +21,32 @@ public class TP2_relation_1_PLAYOUST_VAZQUEZ {
         Voiture uneMicra = new Voiture ("Micra", "Nissan", 4 ) ;
         Personne bob = new Personne("Bobby", "Sixkiller");
         Personne reno = new Personne("Reno", "Raines");
+        Voiture uneFerrari = new Voiture("Spider","Ferrari",8);
         System.out.println("liste des voitures disponibles "+ uneClio +"\n" + uneAutreClio + "\n" + une2008 + "\n" + uneMicra ) ;
+        
+        bob.liste_voitures[0] = uneClio ;
+        bob.nbVoitures = 1 ;
+        uneClio.Proprietaire = bob ;
+        bob.liste_voitures[1] = uneAutreClio ;
+        bob.nbVoitures = 2 ;
+        uneAutreClio.Proprietaire = bob ;
+        
+        reno.liste_voitures[0] = une2008 ;
+        reno.liste_voitures[1] = uneMicra ;
+        reno.nbVoitures = 2 ;
+        une2008.Proprietaire = reno ;
+        uneMicra.Proprietaire = reno ;
+        
+        System.out.println("la premiere voiture de Bob est " +bob.liste_voitures[0] ) ;
+        System.out.println("la deuxieme voiture de Bob est " +bob.liste_voitures[1] ) ;
+        System.out.println("la premiere voiture de Reno est " +reno.liste_voitures[0] ) ;
+        System.out.println("la deuxieme voiture de Reno est " +reno.liste_voitures[1] ) ;
+        boolean res=reno.ajouter_voiture(une2008);
+        System.out.println(res);
+        res=reno.ajouter_voiture(uneFerrari);
+        System.out.println(res);
+        System.out.println("la troisieme voiture de Reno est " +reno.liste_voitures[2] ) ;
     }
+    
     
 }
