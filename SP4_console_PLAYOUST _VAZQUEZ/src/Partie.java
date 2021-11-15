b06b4bc433e38ce13e38814a3fb23377129c6ad0
petitweb <petitweb@192.168.1.7>
2021-11-15 11:56:18 +0100

    SP4 Correction attribuerCouleursAuxJoueurs

--- SP4_console_PLAYOUST _VAZQUEZ/src/Partie.java
+++ SP4_console_PLAYOUST _VAZQUEZ/src/Partie.java
@@ -24,11 +24,12 @@ public class Partie {
      if (ch_couleur==0) {
          
          ListeJoueurs[0].affecterCouleur("Rouge");
+         ListeJoueurs[1].affecterCouleur("Jaune");
          
      } else {
          
-         ListeJoueurs[1].affecterCouleur("Jaune");
-         
+         ListeJoueurs[0].affecterCouleur("Jaune");
+         ListeJoueurs[1].affecterCouleur("Rouge");
               }
      }
      
@@ -38,15 +39,21 @@ public class Partie {
      grilleJeu=new Grille();
      
      attribuerCouleursAuxJoueurs();
-     Jeton jR=new Jeton("Rouge");
-     Jeton jJ=new Jeton("Jaune");
      
      for (int i=0;i<21;i++) {
+         
+        Jeton jR=new Jeton("Rouge");
+        Jeton jJ=new Jeton("Jaune");
+        if (ListeJoueurs[0].Couleur=="Rouge") {
          ListeJoueurs[0].ajouterJeton(jR);
          ListeJoueurs[1].ajouterJeton(jJ);
      }
+        else {
+            ListeJoueurs[1].ajouterJeton(jR);
+            ListeJoueurs[0].ajouterJeton(jJ);
+        }
  }
-     
+ }    
  public void debuterPartie() {
      
     
