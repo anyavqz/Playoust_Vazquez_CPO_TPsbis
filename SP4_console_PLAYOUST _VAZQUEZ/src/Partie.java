37c42aa4c938ed7e6b3be046965a9c2bf20a0a36
petitweb <petitweb@192.168.1.7>
2021-11-15 11:11:46 +0100

    SP4 méthodes attribuer couleurs et initialiser partie

--- SP4_console_PLAYOUST _VAZQUEZ/src/Partie.java
+++ SP4_console_PLAYOUST _VAZQUEZ/src/Partie.java
@@ -2,35 +2,59 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
+import java.util.Random;
 /**
  *
  * @author anyavazquez
  */
 public class Partie {
- 
+    
+    
+ Random generateurAleat = new Random ();
  Joueur ListeJoueurs [] = new Joueur[2];
  Joueur joueurCourant;
- //Grille grilleJeu;
+ Grille grilleJeu;
  
  public Partie(Joueur j1,Joueur j2) {
      ListeJoueurs[0]=j1;
      ListeJoueurs[1]=j2;
 }
  public void attribuerCouleursAuxJoueurs() {
-     int ch_couleur=Math.random();
+     int ch_couleur = generateurAleat.nextInt(1);
      if (ch_couleur==0) {
          
-         ListeJoueurs[0].Couleur="Rouge";
+         ListeJoueurs[0].affecterCouleur("Rouge");
+         
      } else {
-         ListeJoueurs[1].Couleur="Jaune";
+         
+         ListeJoueurs[1].affecterCouleur("Jaune");
+         
               }
      }
      
- }
  
- //public void initialiserPartie() {
+ 
+ public void initialiserPartie() {
+     grilleJeu=new Grille();
      
- //}
+     attribuerCouleursAuxJoueurs();
+     Jeton jR=new Jeton("Rouge");
+     Jeton jJ=new Jeton("Jaune");
+     
+     for (int i=0;i<21;i++) {
+         ListeJoueurs[0].ajouterJeton(jR);
+         ListeJoueurs[1].ajouterJeton(jJ);
+     }
+ }
+     
+ public void debuterPartie() {
+     
+    
+}
+     
+     
+     
+     
  
-//}
+ 
+}
