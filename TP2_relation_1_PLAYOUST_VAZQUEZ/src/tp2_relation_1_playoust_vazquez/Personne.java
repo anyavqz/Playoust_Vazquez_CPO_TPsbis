1f12a0ade77dac82ddbad25bf835543aec3ef140
petitweb <petitweb@192.168.1.7>
2021-11-15 10:00:44 +0100

    TP2 partie 4 finie

--- TP2_relation_1_PLAYOUST_VAZQUEZ/src/tp2_relation_1_playoust_vazquez/Personne.java
+++ TP2_relation_1_PLAYOUST_VAZQUEZ/src/tp2_relation_1_playoust_vazquez/Personne.java
@@ -12,13 +12,31 @@ package tp2_relation_1_playoust_vazquez;
 public class Personne {
     String nom;
     String prenom;
+    int nbVoitures;
+    Voiture liste_voitures [];
     
     
     public Personne (String leNom, String lePrenom) { 
         nom = leNom;
         prenom = lePrenom;
+        nbVoitures=0;
+        liste_voitures = new Voiture[3];
         
     }
+    
+    public boolean ajouter_voiture( Voiture voiture_a_ajouter) {
+            if (voiture_a_ajouter.Proprietaire!=null) {
+                return false;
+            }
+            if (nbVoitures>=3) {
+                return false;
+            }
+            liste_voitures[nbVoitures]=voiture_a_ajouter;
+            nbVoitures+=1;
+            voiture_a_ajouter.Proprietaire = this ;
+            return true;
+            
+}
 
     
     @Override
