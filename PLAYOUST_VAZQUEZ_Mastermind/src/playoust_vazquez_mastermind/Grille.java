014e361150cea2cc896ebec8e5e891cdd79c3802
petitweb <petitweb@192.168.1.7>
2021-12-28 00:54:51 +0100

    Mastermind : Creation dans Grille : AjouterPion, CreerCombi et etreRemplie

--- PLAYOUST_VAZQUEZ_Mastermind/src/playoust_vazquez_mastermind/Grille.java
+++ PLAYOUST_VAZQUEZ_Mastermind/src/playoust_vazquez_mastermind/Grille.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package playoust_vazquez_mastermind;
+import java.util.Scanner;
 
 /**
  *
@@ -12,6 +13,7 @@ package playoust_vazquez_mastermind;
 public class Grille {
 
     Pion PionJeu[][];
+    Scanner sc = new Scanner(System.in);
 
     public Grille() {
         PionJeu = new Pion[12][4];
@@ -26,6 +28,49 @@ public class Grille {
         return PionJeu[l][c].lireCouleur();
     }
     
+    public Pion AjouterPion() {
+        System.out.print("Veuillez choisir une couleur : \n1 : Rose \n2 : Jaune \n3 : Vert \n4 : Bleu\n5 : Orange\n6 : Violet\n7 : Marron\n8 : Turquoise");
+        int ch=sc.nextInt();
+        Pion PionJ= new Pion(null);
+        switch (ch){
+         case 1 :
+             PionJ= new Pion("Rose");
+             break;
+         case 2 : 
+             PionJ =new Pion("Jaune");
+             break;
+         case 3 :
+             PionJ =new Pion("Vert");
+             break;
+         case 4 : 
+             PionJ =new Pion("Bleu");
+             break;
+         case 5 :
+             PionJ =new Pion("Orange");
+             break;
+         case 6 : 
+             PionJ =new Pion("Violet");
+             break;
+         case 7 :
+             PionJ =new Pion("Marron");
+             break;
+         case 8 : 
+             PionJ =new Pion("Turquoise");
+             break;
+        }
+        return PionJ;
+    }
+    
+    public Pion[] CreerCombi() {
+        Pion[] Combi=new Pion[4];
+        
+        System.out.println("Veuillez saisir votre combinaison");
+        Combi[0]=AjouterPion();
+        Combi[1]=AjouterPion();
+        Combi[2]=AjouterPion();
+        Combi[3]=AjouterPion();
+        return Combi;
+    }
     
     public void ajouterCombinaison(Pion[] Combi) {
         int i=0;
@@ -36,6 +81,15 @@ public class Grille {
         for (j=0;j<4;j++) {
             PionJeu[i][j]=Combi[j];
         }
+    
+    }
+    
+    public boolean etreRemplie() {
+        boolean etat=false;
+        if (PionJeu[12][0]!=null) {
+            etat=true;
+        }
+        return etat;
     }
     
     public void afficherGrillesurConsole() {
@@ -75,6 +129,7 @@ public class Grille {
     }
 }
 }
+
 
 
     
