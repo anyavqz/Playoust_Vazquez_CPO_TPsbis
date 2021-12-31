e6c4e1dbb139b735c60873ae95c2aaa25b762be5
anyavazquez <anyavazquez@192.168.114.142>
2021-12-31 11:32:20 +0100

    Création du mode Graphique pour matsermind

---
+++ PLAYOUST_VAZQUEZ_Mastermind_Graphique/src/playoust_vazquez_mastermind/Joueur.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package playoust_vazquez_mastermind;
+
+/**
+ *
+ * @author anyavazquez
+ */
+public class Joueur {
+    String Nom;
+    Pion ListePions [] = new Pion [480];
+    
+    public Joueur (String nomJ){
+        Nom=nomJ;
+    }
+    
+    
+    
+    
+}
---
+++ PLAYOUST_VAZQUEZ_Mastermind_Graphique/src/playoust_vazquez_mastermind/PLAYOUST_VAZQUEZ_Mastermind.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package playoust_vazquez_mastermind;
+
+/**
+ *
+ * @author anyavazquez
+ */
+public class PLAYOUST_VAZQUEZ_Mastermind {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Joueur J1=new Joueur("prosper");
+        Partie Partie1= new Partie();
+        Partie1.debuterPartie();
+        
+        }
+               
+    }
+    
+
