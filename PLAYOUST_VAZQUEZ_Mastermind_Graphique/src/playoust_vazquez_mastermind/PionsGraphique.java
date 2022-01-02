11166fd18ecee3a4c5e02526099164d18add14b2
petitweb <petitweb@192.168.1.7>
2022-01-02 22:45:23 +0100

    Mastermind : Elaboration de PionsGraphique

--- PLAYOUST_VAZQUEZ_Mastermind_Graphique/src/playoust_vazquez_mastermind/PionsGraphique.java
+++ PLAYOUST_VAZQUEZ_Mastermind_Graphique/src/playoust_vazquez_mastermind/PionsGraphique.java
@@ -4,6 +4,8 @@
  */
 package playoust_vazquez_mastermind;
 
+import java.awt.Graphics;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
 /**
@@ -12,8 +14,45 @@ import javax.swing.JButton;
  */
 public class PionsGraphique extends JButton{
     Pion PionAssocie;
+    ImageIcon img_turquoise = new javax.swing.ImageIcon(getClass().getResource("/imagesMastermind/T.jpeg"));
+    ImageIcon img_bleue = new javax.swing.ImageIcon(getClass().getResource("/imagesMastermind/B.jpeg"));
+    ImageIcon img_grise = new javax.swing.ImageIcon(getClass().getResource("/imagesMastermind/G.jpeg"));
+    ImageIcon img_jaune = new javax.swing.ImageIcon(getClass().getResource("/imagesMastermind/J.jpeg"));
+    ImageIcon img_marron = new javax.swing.ImageIcon(getClass().getResource("/imagesMastermind/M.jpeg"));
+    ImageIcon img_orange = new javax.swing.ImageIcon(getClass().getResource("/imagesMastermind/O.jpeg"));
+    ImageIcon img_verte = new javax.swing.ImageIcon(getClass().getResource("/imagesMastermind/V.jpeg"));
+    ImageIcon img_rose = new javax.swing.ImageIcon(getClass().getResource("/imagesMastermind/R.jpeg"));
     
     public PionsGraphique (Pion unPion) {
         PionAssocie = unPion;
+    }
+    
+    @Override
+    public void paintComponent (Graphics G) {
+        super.paintComponent(G);
+        if (PionAssocie.lireCouleur()=="Turquoise") {
+            setIcon(img_turquoise);
+        }
+        if (PionAssocie.lireCouleur()=="Orange") {
+            setIcon(img_orange);
+        }
+        if (PionAssocie.lireCouleur()=="Gris") {
+            setIcon(img_grise);
+        }
+        if (PionAssocie.lireCouleur()=="Jaune") {
+            setIcon(img_jaune);
+        }
+        if (PionAssocie.lireCouleur()=="Marron") {
+            setIcon(img_marron);
+        }
+        if (PionAssocie.lireCouleur()=="Vert") {
+            setIcon(img_verte);
+        }
+        if (PionAssocie.lireCouleur()=="Rose") {
+            setIcon(img_rose);
+        }
+        if (PionAssocie.lireCouleur()=="Bleu") {
+            setIcon(img_bleue);
+        }
     }
 }
